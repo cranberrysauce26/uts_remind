7b361299888cb87b4c021e6012873b86b1d10d46
Steven Mai <stevenmai132@yahoo.ca>
2017-05-09 18:24:08 -0400

    stoped testing

--- app.js
+++ app.js
@@ -30,36 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // using the routeres
 
 app.use('/', index);
-// app.use('/webhook', webhook);
-
-// TESTING
-var conversation = require('./conversation');
-
-app.get('/', function (req, res) {
-    console.log("GET call to webhook");
-    if (req.query["hub.verify_token"] === process.env.VERIFY_TOKEN) {
-        console.log("Verified webhook");
-        res.status(200).send(req.query["hub.challenge"]);
-    } else {
-        console.error("Verification failed. The tokens do not match.");
-        res.sendStatus(403);
-    }
-});
-
-app.post('/', function (req, res) {
-    console.log("POST call to webhook");
-
-    var data = req.body;
-
-    if (data.object === 'page') {
-        data.entry.forEach(function (messagingEvent) {
-            conversation.respond(messagingEvent);
-        });
-        res.sendStatus(200);
-    }
-});
-// END TESTING
-
+app.use('/webhook', webhook);
 app.use('/authorize', authorize);
 
 // catch 404 and forward to error handler

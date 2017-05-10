e19f76c30c4a305e659f93be8d5d7080c4948a4b
Steven Mai <stevenmai132@yahoo.ca>
2017-05-10 12:16:11 -0400

    uri to url send

--- facebook/send.js
+++ facebook/send.js
@@ -6,7 +6,7 @@ function send(messageData) {
     console.log("In send with messageData==");
     console.log(JSON.stringify(messageData));
     request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
         method: 'POST',
         json: messageData

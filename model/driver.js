74a4b1317043671e198b1962615fe1fb68a2ddca
Steven Mai <stevenmai132@yahoo.ca>
2017-05-13 10:05:02 -0400

    fixed another error in neoj4

--- model/driver.js
+++ model/driver.js
@@ -2,7 +2,7 @@
 
 const neo4j = require('neo4j-driver').v1;
 
-var driver = neo4j.driver(process.env.GRAPHENEDB_BOLT_URL, neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, GRAPHENEDB_BOLT_PASSWORD));
+var driver = neo4j.driver(process.env.GRAPHENEDB_BOLT_URL, neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD));
 
 driver.onCompleted = function() {
     console.log("Succesfully configured graphene db driver");

9646e6e74ac512be516d135ca7bba5849423f0b1
Steven Mai <stevenmai132@yahoo.ca>
2017-05-13 10:03:07 -0400

    mistake in configuring neoj4 fixed

--- model/driver.js
+++ model/driver.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const neo4j = require('neo4j-driver');
+const neo4j = require('neo4j-driver').v1;
 
 var driver = neo4j.driver(process.env.GRAPHENEDB_BOLT_URL, neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, GRAPHENEDB_BOLT_PASSWORD));
 
